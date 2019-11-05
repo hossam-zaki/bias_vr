ecae596c44a0f31a078e885ed7d27133468d8152
bryan <bryandrm97@gmail.com>
2019-11-04 18:23:44 -0600

    added animation canvas , an interactive button, GUI for the signin,  Gui before the video begins

---
+++ BiasCoffeeShop/Assets/PopUpWindows/Scripts/AcctionButton.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AcctionButton : MonoBehaviour
+{
+    public Animator animator;
+    public Canvas canvas;
+    public Animator Videoanimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {   
+        if(other.tag=="Player"){
+                        
+            animator.SetBool("Active", true);
+        }
+        
+    }
+
+    public void PlayVideo(){
+        animator.SetBool("Active", false);
+        Videoanimator.SetBool("DisplayVideo", true);
+    }
+    
+    public void CancelVideo(){
+        animator.SetBool("Active", false);
+    }
+    
+}
---
+++ BiasCoffeeShop/Assets/PopUpWindows/Scripts/MainPopup.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainPopup : MonoBehaviour
+{
+
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ClosePanle()
+    {
+        animator.SetBool("play", false);
+    }
+}
+
+
